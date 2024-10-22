d188a33403a54613b2fbda743428c3177d80bcb8
Ubuntu <ubuntu@ip-172-31-23-194.ec2.internal>
2024-10-22 20:04:09 +0000

    test3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_16_085127) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_085127) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
@@ -32,4 +32,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_16_085127) do
     t.datetime "updated_at", null: false
     t.integer "age"
   end
+
 end
